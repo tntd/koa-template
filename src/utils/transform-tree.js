b14b6ffe567115479e48b8a9c94013afc924d5f2
Anthony Li <lisang881021@sina.com>
2021-03-22 14:38:20 +0800

    fix:优化tree 处理

--- src/utils/transform-tree.js
+++ src/utils/transform-tree.js
@@ -1,39 +1,49 @@
 'use strict';
 
-module.exports = {
-	transformTree(list, options = {}) {
-		const { keyField = 'id', childField = 'children', parentField = 'parentId' } = options;
+const transformTree = (list, options = {}) => {
+	const {
+		keyField = 'id',
+		childField = 'children',
+		parentField = 'parentId',
+		formatData
+	} = options;
 
-		const tree = [];
-		const record = {};
+	const tree = [];
+	const record = {};
 
-		for (let i = 0, len = list.length; i < len; i++) {
-			const item = list[i].dataValues || list[i];
-			const id = item[keyField];
+	for (let i = 0, len = list.length; i < len; i++) {
+		let item = list[i].dataValues || list[i];
+		const id = item[keyField];
 
-			if (!id) {
-				continue;
-			}
-
-			if (record[id]) {
-				item[childField] = record[id];
-			} else {
-				item[childField] = record[id] = [];
-			}
-
-			if (item[parentField]) {
-				const parentId = item[parentField];
-
-				if (!record[parentId]) {
-					record[parentId] = [];
-				}
-
-				record[parentId].push(item);
-			} else {
-				tree.push(item);
-			}
+		if (!id) {
+			continue;
 		}
 
-		return tree;
+		if (record[id]) {
+			item[childField] = record[id];
+		} else {
+			item[childField] = record[id] = [];
+		}
+
+		if (item[parentField]) {
+			const parentId = item[parentField];
+
+			if (!record[parentId]) {
+				record[parentId] = [];
+			}
+			if (formatData) {
+				item = formatData(item);
+			}
+			record[parentId].push(item);
+		} else {
+			if (formatData) {
+				item = formatData(item);
+			}
+			tree.push(item);
+		}
 	}
+
+	return tree;
 };
+
+module.exports = transformTree;
